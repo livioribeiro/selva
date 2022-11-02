886a60af2ef6bfaf4706636af222797827b49e08
Livio Ribeiro <livio@codata.pb.gov.br>
2022-11-02 16:15:50 -0300

    add environment helpers

---
+++ src/selva/configuration/environment.py
@@ -0,0 +1,93 @@
+import json
+import os
+from collections.abc import Callable, Mapping, Sequence
+from typing import TypeVar, Any, Optional
+
+T = TypeVar("T")
+
+_UNSET = ()
+
+
+def get_str(key: str, default: Optional[str] = _UNSET) -> str:
+    if value := os.getenv(key):
+        return value
+
+    if default is not _UNSET:
+        return default
+
+    raise KeyError(f"Environment variable '{key}' is not defined")
+
+
+def _get_env_and_convert(
+    key: str, default: Optional[T], converter: Callable[[str], T], type_name: str
+) -> T:
+    value = get_str(key, default)
+
+    try:
+        return converter(value)
+    except ValueError as err:
+        message = (
+            f"Environment variable '{key}'"
+            f" is not compatible with type '{type_name}': '{value}'"
+        )
+        raise ValueError(message) from err
+
+
+def get_int(key: str, default: Optional[int] = _UNSET) -> int:
+    return _get_env_and_convert(key, default, int, "int")
+
+
+def get_float(key: str, default: Optional[float] = _UNSET) -> float:
+    return _get_env_and_convert(key, default, float, "float")
+
+
+def _bool_converter(value: str) -> bool:
+    if value in ("1", "true", "True"):
+        return True
+
+    if value in ("0", "false", "False"):
+        return False
+
+    raise ValueError(
+        "bool value must be one of ['1', 'true', 'True', '0', 'false', 'False']"
+    )
+
+
+def get_bool(key: str, default: Optional[bool] = _UNSET) -> bool:
+    return _get_env_and_convert(key, default, _bool_converter, "bool")
+
+
+def _list_converter(value: str) -> list[str]:
+    return [v.strip().strip("'\"") for v in value.split(",")]
+
+
+def get_list(key: str, default: Optional[Sequence[str]] = _UNSET) -> Sequence[str]:
+    return _get_env_and_convert(key, default, _list_converter, "list")
+
+
+def _dict_converter(value: str) -> dict[str, str]:
+    result: dict[str, str] = {}
+
+    for item in value.split(","):
+        match item.split("=", maxsplit=1):
+            case [key, val]:
+                key = key.strip().strip("'\"")
+                val = val.strip().strip("'\"")
+                result[key] = val
+            case _:
+                message = f"{item} is not a valid dict item"
+                raise ValueError(message)
+
+    return result
+
+
+def get_dict(key, default: Optional[Mapping[str, str]] = _UNSET) -> Mapping[str, str]:
+    return _get_env_and_convert(key, default, _dict_converter, "dict")
+
+
+def _json_converter(value: str) -> dict[str, Any]:
+    return json.loads(value)
+
+
+def get_json(key, default: Optional[Mapping[str, Any]] = _UNSET) -> Mapping[str, Any]:
+    return _get_env_and_convert(key, default, _json_converter, "json")
---
+++ tests/configuration/test_environ.py
@@ -0,0 +1,183 @@
+import pytest
+
+from selva.configuration import environment as env
+
+
+def test_get_str(monkeypatch):
+    monkeypatch.setenv("VALUE", "str")
+    result = env.get_str("VALUE")
+    assert result == "str"
+
+
+def test_get_int(monkeypatch):
+    monkeypatch.setenv("VALUE", "123")
+    result = env.get_int("VALUE")
+    assert result == 123
+
+
+def test_get_int_with_invalid_value_should_fail(monkeypatch):
+    monkeypatch.setenv("VALUE", "abc")
+    with pytest.raises(ValueError):
+        env.get_int("VALUE")
+
+
+def test_get_float(monkeypatch):
+    monkeypatch.setenv("VALUE", "1.23")
+    result = env.get_float("VALUE")
+    assert result == 1.23
+
+
+def test_get_float_with_invalid_should_fail(monkeypatch):
+    monkeypatch.setenv("VALUE", "abc")
+    with pytest.raises(ValueError):
+        env.get_float("VALUE")
+
+
+@pytest.mark.parametrize(
+    "raw,expected",
+    [
+        ("true", True),
+        ("True", True),
+        ("1", True),
+        ("false", False),
+        ("False", False),
+        ("0", False),
+    ],
+    ids=["true", "True", "1", "false", "False", "0"]
+)
+def test_get_bool(raw, expected, monkeypatch):
+    monkeypatch.setenv("VALUE", raw)
+    result = env.get_bool("VALUE")
+    assert result == expected
+
+
+def test_get_bool_with_invalid_should_fail(monkeypatch):
+    monkeypatch.setenv("VALUE", "abc")
+    with pytest.raises(ValueError):
+        env.get_bool("VALUE")
+
+
+@pytest.mark.parametrize(
+    "raw,expected",
+    [
+        ("abc", ["abc"]),
+        ("abc,", ["abc", ""]),
+        ("abc,,", ["abc", "", ""]),
+        (",abc", ["", "abc"]),
+        (",,abc", ["", "", "abc"]),
+        ("abc,def,ghi", ["abc", "def", "ghi"]),
+        ('"abc","def","ghi"', ["abc", "def", "ghi"]),
+        ("'abc','def','ghi'", ["abc", "def", "ghi"]),
+        ('"abc",def,ghi', ["abc", "def", "ghi"]),
+        ("'abc',def,ghi", ["abc", "def", "ghi"]),
+        (" abc,def , ghi ", ["abc", "def", "ghi"]),
+        ("'abc ',' def',' ghi '", ["abc ", " def", " ghi "]),
+        (" 'abc ',' def', ' ghi ' ", ["abc ", " def", " ghi "]),
+    ],
+    ids=[
+        "abc",
+        "abc,",
+        "abc,,",
+        ",abc",
+        ",,abc",
+        "abc,def,ghi",
+        '"abc","def","ghi"',
+        "'abc','def','ghi'",
+        '"abc",def,ghi',
+        "'abc',def,ghi",
+        " abc,def , ghi ",
+        "'abc ',' def',' ghi '",
+        " 'abc ',' def', ' ghi ' ",
+    ]
+)
+def test_get_list(raw, expected, monkeypatch):
+    monkeypatch.setenv("VALUE", raw)
+    result = env.get_list("VALUE")
+    assert result == expected
+
+
+@pytest.mark.parametrize(
+    "raw,expected",
+    [
+        ("a=1", {"a": "1"}),
+        ("a=1,b=2", {"a": "1", "b": "2"}),
+        ("'a'=1,b='2','c'='3'", {"a": "1", "b": "2", "c": "3"}),
+        ('"a"=1,b="2","c"="3"', {"a": "1", "b": "2", "c": "3"}),
+        ("'a'=\"1\",\"b\"='2','c'='3',\"d\"=\"4\"", {"a": "1", "b": "2", "c": "3", "d": "4"}),
+        ("a =1 , b= 2, c = 3 ", {"a": "1", "b": "2", "c": "3"}),
+        ("'a '='1 ',' b'=' 2',' c '=' 3 '", {"a ": "1 ", " b": " 2", " c ": " 3 "}),
+    ],
+    ids=[
+        "a=1",
+        "a=1,b=2",
+        "'a'=1,b='2','c'='3'",
+        '"a"=1,b="2","c"="3"',
+        "'a'=\"1\",\"b\"='2','c'='3',\"d\"=\"4\"",
+        "a =1 , b= 2, c = 3 ",
+        "'a '='1 ',' b'=' 2',' c '=' 3 '",
+    ]
+)
+def test_get_dict(raw, expected, monkeypatch):
+    monkeypatch.setenv("VALUE", raw)
+    result = env.get_dict("VALUE")
+    assert result == expected
+
+
+@pytest.mark.parametrize(
+    "value",
+    [
+        "a",
+        "a=",
+        "a=1,",
+        ",a=1"
+    ]
+)
+def test_get_dict_with_invalid_should_fail(value, monkeypatch):
+    monkeypatch.setenv("VALUE", value)
+    with pytest.raises(ValueError):
+        env.get_bool("VALUE")
+
+
+@pytest.mark.parametrize(
+    "raw,expected",
+    [
+        ("1", 1),
+        ("1.2", 1.2),
+        ("true", True),
+        ("false", False),
+        ("null", None),
+        ('"1"', "1"),
+        ('"1.2"', "1.2"),
+        ('"true"', "true"),
+        ('"false"', "false"),
+        ('"null"', "null"),
+        ("[1, 2, 3]", [1, 2, 3]),
+        ('["a", "b", "c"]', ["a", "b", "c"]),
+        ('{"a": 1, "b": 1.2, "c": "3", "d": true, "e": null}', {"a": 1, "b": 1.2, "c": "3", "d": True, "e": None})
+    ],
+    ids=[
+        "1",
+        "1.2",
+        "true",
+        "false",
+        "null",
+        '"1"',
+        '"1.2"',
+        '"true"',
+        '"false"',
+        '"null"',
+        "[1, 2, 3]",
+        '["a", "b", "c"]',
+        '{"a": 1, "b": 1.2, "c": "3", "d": true, "e": null}',
+    ]
+)
+def test_get_json(raw, expected, monkeypatch):
+    monkeypatch.setenv("VALUE", raw)
+    result = env.get_json("VALUE")
+    assert result == expected
+
+
+def test_get_json_with_invalid_should_fail(monkeypatch):
+    monkeypatch.setenv("VALUE", "abc")
+    with pytest.raises(ValueError):
+        env.get_bool("VALUE")
