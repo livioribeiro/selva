fd15395fe054768a44d87c544e54a381ab0c8343
Livio Ribeiro <livio@codata.pb.gov.br>
2022-12-26 09:54:19 -0300

    improve http errors

--- src/selva/web/errors.py
+++ src/selva/web/errors.py
@@ -2,66 +2,35 @@ from http import HTTPStatus
 
 from starlette.exceptions import HTTPException, WebSocketException
 
-
-class HTTPClientError(HTTPException):
-    def __init__(
-        self,
-        status_code: HTTPStatus,
-        reason: str | None = None,
-        headers: dict[str, str] | None = None,
-    ):
-        if not (400 <= status_code < 500):
-            raise ValueError(
-                f"{self.__class__.__name__} status_code should be in '400' range"
-            )
-
-        super().__init__(status_code, reason, headers)
-
-
-class HTTPBadRequestError(HTTPClientError):
+class HTTPBadRequestError(HTTPException):
     def __init__(
         self, reason: str | None = None, headers: dict[str, str] | None = None
     ):
         super().__init__(HTTPStatus.BAD_REQUEST, reason, headers)
 
 
-class HTTPNotFoundError(HTTPClientError):
+class HTTPNotFoundError(HTTPException):
     def __init__(
         self, reason: str | None = None, headers: dict[str, str] | None = None
     ):
         super().__init__(HTTPStatus.NOT_FOUND, reason, headers)
 
 
-class HTTPUnauthorizedError(HTTPClientError):
+class HTTPUnauthorizedError(HTTPException):
     def __init__(
         self, reason: str | None = None, headers: dict[str, str] | None = None
     ):
         super().__init__(HTTPStatus.UNAUTHORIZED, reason, headers)
 
 
-class HTTPForbiddenError(HTTPClientError):
+class HTTPForbiddenError(HTTPException):
     def __init__(
         self, reason: str | None = None, headers: dict[str, str] | None = None
     ):
         super().__init__(HTTPStatus.FORBIDDEN, reason, headers)
 
 
-class HTTPServerError(HTTPException):
-    def __init__(
-        self,
-        status_code: HTTPStatus,
-        reason: str | None = None,
-        headers: dict[str, str] | None = None,
-    ):
-        if not (500 <= status_code < 600):
-            raise ValueError(
-                f"{self.__class__.__name__} status_code should be in '500' range"
-            )
-
-        super().__init__(status_code, reason, headers)
-
-
-class HTTPInternalServerError(HTTPServerError):
+class HTTPInternalServerError(HTTPException):
     def __init__(
         self, reason: str | None = None, headers: dict[str, str] | None = None
     ):
--- tests/web/test_errors.py
+++ tests/web/test_errors.py
@@ -4,11 +4,9 @@ import pytest
 
 from selva.web.errors import (
     HTTPBadRequestError,
-    HTTPClientError,
     HTTPForbiddenError,
     HTTPInternalServerError,
     HTTPNotFoundError,
-    HTTPServerError,
     HTTPUnauthorizedError,
 )
 
@@ -33,17 +31,3 @@ from selva.web.errors import (
 def test_correct_status_code(error_cls, status: HTTPStatus):
     error = error_cls()
     assert error.status_code == status
-
-
-@pytest.mark.parametrize(
-    "error_cls, status_code",
-    [
-        (HTTPClientError, 300),
-        (HTTPClientError, 500),
-        (HTTPServerError, 400),
-        (HTTPServerError, 600),
-    ],
-)
-def test_base_error_with_wrong_status_code_should_raise_error(error_cls, status_code):
-    with pytest.raises(ValueError):
-        error_cls(status_code)
