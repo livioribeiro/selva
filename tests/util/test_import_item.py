4290446347944e28f6f0f94bbe8350edae995406
Livio Ribeiro <livioribeiro@outlook.com>
2023-11-02 09:07:07 -0300

    add tests for _util.import_item

---
+++ tests/util/test_import_item.py
@@ -0,0 +1,17 @@
+import pytest
+
+from selva._util.import_item import import_item
+
+
+def test_import_item():
+    item = import_item("tests.util.package_to_scan.module_to_scan.ClassItem")
+
+    from .package_to_scan.module_to_scan import ClassItem
+
+    assert item is ClassItem
+
+
+@pytest.mark.parametrize("name", ["invalid", "invalid.module:item", ""])
+def test_import_item_invalid_name_should_fail(name):
+    with pytest.raises(ValueError, match="name must be in 'module.item' format"):
+        import_item("invalid")
