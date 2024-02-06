1115d038bb7a78b29967b438f82f82de21de256e
Livio Ribeiro <livioribeiro@outlook.com>
2024-02-06 13:46:54 -0300

    test: add tests for sqlalchemy options

--- tests/contrib/data/sqlalchemy/test_service.py
+++ tests/contrib/data/sqlalchemy/test_service.py
@@ -5,7 +5,7 @@ from sqlalchemy import text
 
 from selva.configuration.defaults import default_settings
 from selva.configuration.settings import Settings
-from selva.contrib.data.sqlalchemy.service import make_engine_service
+from selva.contrib.data.sqlalchemy.service import make_engine_service, make_sessionmaker_service
 
 
 async def _test_engine_service(settings: Settings):
@@ -46,6 +46,53 @@ async def test_make_engine_service_with_url_components():
     })
 
     await _test_engine_service(settings)
+
+
+async def test_make_engine_service_with_options():
+    settings = Settings(default_settings | {
+        "data": {
+            "sqlalchemy": {
+                "default": {
+                    "url": "sqlite+aiosqlite:///:memory:",
+                    "options": {
+                        "echo": True,
+                        "echo_pool": True,
+                    },
+                },
+            },
+        },
+    })
+
+    engine_service = make_engine_service("default")(settings)
+    engine = await anext(engine_service)
+    assert engine.echo is True
+    assert engine.pool.echo is True
+
+
+async def test_make_engine_service_with_execution_options():
+    settings = Settings(default_settings | {
+        "data": {
+            "sqlalchemy": {
+                "default": {
+                    "url": "sqlite+aiosqlite:///:memory:",
+                    "options": {
+                        "execution_options": {
+                            "isolation_level": "READ UNCOMMITTED"
+                        },
+                    },
+                },
+            },
+        },
+    })
+
+    engine_service = make_engine_service("default")
+    engine = await anext(engine_service(settings))
+    sessionmaker_service = make_sessionmaker_service("default")
+    sessionmaker = await sessionmaker_service(engine)
+
+    async with sessionmaker() as session:
+        result = await session.execute(text("select 1"))
+        assert result.context.execution_options["isolation_level"] == "READ UNCOMMITTED"
 
 
 @pytest.mark.skipif(find_spec("psycopg") is None, reason="psycopg not present")
@@ -108,7 +155,7 @@ async def test_postgres_make_engine_service_with_url_and_options():
                 "default": {
                     "url": "postgresql+psycopg://postgres:postgres@localhost:5432/postgres",
                     "options": {
-                        "pool_size": 1,
+                        "echo": True,
                         "echo_pool": True,
                     }
                 },
@@ -118,4 +165,5 @@ async def test_postgres_make_engine_service_with_url_and_options():
 
     engine_service = make_engine_service("default")(settings)
     engine = await anext(engine_service)
-    assert engine is not None
+    assert engine.echo is True
+    assert engine.pool.echo is True
